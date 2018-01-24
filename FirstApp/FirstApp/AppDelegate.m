56c93211d2531ad2277d5d10a7308b8ff687f692
Satish muttavarapu <muttavarapus@gmail.com>
2018-01-24 23:15:56 +0530

    fourth

--- FirstApp/FirstApp/AppDelegate.m
+++ FirstApp/FirstApp/AppDelegate.m
@@ -18,6 +18,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"Some Changes");
+    NSLog(@"Another Changes");
+
     return YES;
 }
 
